5cacd71e34c18ba6ff8298d9bf8c6b3a6be9bada
minhuang0 <huangmin0822@163.com>
2016-07-22 18:35:38 +0800

    debug index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
 var markdown = require('gulp-markdown');
+
+gulp.task('connect', function() {
+  connect.server({
+    port: 8888
+  });
+});
 
 gulp.task('default', function () {
     return gulp.src('README.md')
